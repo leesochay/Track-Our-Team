8121ff5abc593a7848331f9fe03c1b4f1b26d65d
leesochay <sochaylee@gmail.com>
2023-06-25 20:15:39 -0400

    initial inquirer and sql code

---
+++ delete.sql
@@ -0,0 +1,8 @@
+DELETE FROM department
+WHERE id = 1;
+
+DELETE FROM role
+WHERE id = 1;
+
+DELETE FROM employee
+WHERE id = 1;
---
+++ update.sql
@@ -0,0 +1,11 @@
+UPDATE department
+SET name = "name"
+WHERE id = 1;
+
+UPDATE role
+SET name = "title"
+WHERE id = 1;
+
+UPDATE employee
+SET name = "first_name"
+WHERE id = 1;
